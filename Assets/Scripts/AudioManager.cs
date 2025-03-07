16d326f7de3911b37ad3001613bcd58bb148b05d
ImSupaBob <artemtokarev2@gmail.com>
2025-03-07 21:32:05 +0100

    fix sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     [Header("Audio Source")]
     [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource ambientSource;
     [SerializeField] private AudioSource sfxSource;
     
     [Header("Audio Clips")]
@@ -14,13 +16,61 @@ public class AudioManager : MonoBehaviour
     public AudioClip blackHole;
     public AudioClip automation;
     public AudioClip nextStage;
-
+    
+    [Header("Settings")]
+    public float fadeDuration = 2f;
     private void Start()
     {
-        musicSource.clip = ambient;
-        musicSource.Play();
+        ambientSource.clip = ambient;
+        ambientSource.Play();
+        StartCoroutine(PlayMainThemeAfterDelay(5f));
+        StartCoroutine(PlayMainThemeAfterDelay(300f));
+    }
+    
+    private IEnumerator PlayMainThemeAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        yield return StartCoroutine(FadeIn(musicSource, fadeDuration));
+
+
+        PlayMusic();
+
+
+        yield return new WaitForSeconds(mainTheme.length); // Учитываем время на затухание
+        yield return StartCoroutine(FadeOut(musicSource, fadeDuration));
     }
 
+    private IEnumerator FadeOut(AudioSource source, float duration)
+    {
+        float startVolume = source.volume;
+        float time = 0;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            source.volume = Mathf.Lerp(startVolume, 0, time / duration);
+            yield return null;
+        }
+
+        source.volume = 0;
+    }
+
+    private IEnumerator FadeIn(AudioSource source, float duration)
+    {
+        float time = 0;
+        source.volume = 0;
+        float targetVolume = 0.1f; // Стандартная громкость
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            source.volume = Mathf.Lerp(0, targetVolume, time / duration);
+            yield return null;
+        }
+
+        source.volume = targetVolume;
+    }
+    
     public void PlayMusic()
     {
         musicSource.clip = mainTheme;
