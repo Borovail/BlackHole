77ebf5c2b13acd19bae30be470066401d86b72cf
ImSupaBob <artemtokarev2@gmail.com>
2025-03-09 00:36:38 +0100

    update audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -26,57 +26,20 @@ public class AudioManager : Singleton<AudioManager>
         _ambientSource.clip = Ambient;
         _ambientSource.Play();
         StartCoroutine(PlayMainThemeAfterDelay(5f));
-        StartCoroutine(PlayMainThemeAfterDelay(300f));
     }
     
     private IEnumerator PlayMainThemeAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-        yield return StartCoroutine(FadeIn(_musicSource, FadeDuration));
-
-
+        
         PlayMusic();
-
-
-        yield return new WaitForSeconds(MainTheme.length); // Учитываем время на затухание
-        yield return StartCoroutine(FadeOut(_musicSource, FadeDuration));
-    }
-
-    private IEnumerator FadeOut(AudioSource source, float duration)
-    {
-        float startVolume = source.volume;
-        float time = 0;
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            source.volume = Mathf.Lerp(startVolume, 0, time / duration);
-            yield return null;
-        }
-
-        source.volume = 0;
-    }
-
-    private IEnumerator FadeIn(AudioSource source, float duration)
-    {
-        float time = 0;
-        source.volume = 0;
-        float targetVolume = 0.5f; // Стандартная громкость
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            source.volume = Mathf.Lerp(0, targetVolume, time / duration);
-            yield return null;
-        }
-
-        source.volume = targetVolume;
     }
     
     public void PlayMusic()
     {
         _musicSource.clip = MainTheme;
         _musicSource.Play();
+        _musicSource.loop = true;
     }
 
     public void PlaySfx(AudioClip clip)
